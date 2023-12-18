cd572b6ccf5e2c6a9859c0375f0d722183088ff8
varhan1 <136239791+varhan1@users.noreply.github.com>
2023-12-18 08:07:55 +0700

    Update js.js

--- js.js
+++ js.js
@@ -1,9 +1,31 @@
-let register_btn = document.querySelector(".Register-btn");
-let login_btn = document.querySelector(".Login-btn");
-let form = document.querySelector(".Form-box");
-register_btn.addEventListener("click", () => {
-    form.classList.add("change-form");
+const navbarMenu = document.querySelector(".navbar .links");
+const hamburgerBtn = document.querySelector(".hamburger-btn");
+const hideMenuBtn = navbarMenu.querySelector(".close-btn");
+const showPopupBtn = document.querySelector(".login-btn");
+const formPopup = document.querySelector(".form-popup");
+const hidePopupBtn = formPopup.querySelector(".close-btn");
+const signupLoginLink = formPopup.querySelectorAll(".bottom-link a");
+
+// Show mobile menu
+hamburgerBtn.addEventListener("click", () => {
+    navbarMenu.classList.toggle("show-menu");
 });
-login_btn.addEventListener("click", () => {
-    form.classList.remove("change-form");
+
+// Hide mobile menu
+hideMenuBtn.addEventListener("click", () =>  hamburgerBtn.click());
+
+// Show login popup
+showPopupBtn.addEventListener("click", () => {
+    document.body.classList.toggle("show-popup");
+});
+
+// Hide login popup
+hidePopupBtn.addEventListener("click", () => showPopupBtn.click());
+
+// Show or hide signup form
+signupLoginLink.forEach(link => {
+    link.addEventListener("click", (e) => {
+        e.preventDefault();
+        formPopup.classList[link.id === 'signup-link' ? 'add' : 'remove']("show-signup");
+    });
 });
